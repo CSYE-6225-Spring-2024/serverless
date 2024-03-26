7f04f1384da9151fe9b955fd495bd7fd85c36a6b
Anirban Dutta <dutta.an@northeastern.edu>
2024-03-25 20:48:58 -0400

    Sync and add enteries to a new table for email tracking

--- index.js
+++ index.js
@@ -1,38 +1,92 @@
 const cloudFunction = require("@google-cloud/functions-framework");
 const mailgun = require("mailgun-js");
 const jwt = require("jsonwebtoken");
+const { Sequelize } = require("sequelize");
+const { DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PWD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+
+const UserVerified = sequelize.define(
+  "user_verification",
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { isEmail: true },
+    },
+    email_sent_time: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    },
+    email_verified_time: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 cloudFunction.cloudEvent("sendEmail", async (cloudEvent) => {
   try {
     const base64name = cloudEvent.data.message.data;
     const userData = Buffer.from(base64name, "base64").toString();
-    const jsonData = JSON.parse(userData);
     if (!userData) {
       console.error("Invalid Pub/Sub message received.");
       return;
     }
-    console.log(`Data received from pubsub:', ${jsonData}`);
-    const token = await createToken(jsonData);
-    if (token) {
-      await sendEmail(token, jsonData.username);
+    const jsonData = JSON.parse(userData);
+    console.log("Data received from pubsub:", jsonData.username);
+    const isDBUp = await checkDBConnection();
+    if (isDBUp) {
+      const token = await createToken(jsonData);
+      if (token) {
+        await sendEmail(token, jsonData.username);
+      }
     }
   } catch (error) {
-    console.error("Error processing mailgun", JSON.stringify(error));
+    var errorLog = { errorLog: error };
+    console.error("Error processing mailgun", errorLog);
   }
 });
 
+async function checkDBConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Database is running");
+    return true;
+  } catch (error) {
+    var errorDBLog = { errorDBLog: error };
+    console.log("Database failed to run", errorDBLog);
+    return false;
+  }
+}
+
 async function createToken(userDetails) {
-  const currTime = Math.floor(Date.now() / 1000);
-  const expiryTime = currTime + 120;
-  const token = jwt.sign(
-    {
-      username: userDetails.username,
-      timestamp: currTime,
-      exp: expiryTime,
-    },
-    "csye6225-webapp"
-  );
-  return token;
+  await UserVerified.sync();
+  const new_user = await UserVerified.create({
+    username: userDetails.username,
+  });
+  console.log("User details from DB:", new_user);
+  return new_user.id;
 }
 
 async function sendEmail(token, username) {
